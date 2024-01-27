375e9f15a5e47f384aebaba5665f4c48c454546a
Paul <138paulmiller@gmail.com>
2024-01-26 21:12:45 -0800

    C testing framework

--- test/ctest.sh
+++ test/ctest.sh
@@ -4,4 +4,13 @@ rm -r ../build/
 mkdir ../build
 gcc -g -o ../build/augc main.c -I../ -lm 
 valgrind --main-stacksize=1048576 --tool=memcheck --leak-check=full \
-../build/augc ../examples/test
+../build/augc                   \
+../examples/test                \
+../examples/test_fib            \
+../examples/test_fib_recursive  \
+../examples/test_binops         \
+../examples/test_func           \
+../examples/test_func_callbacks \
+../examples/test_func_local     \
+../examples/test_stackoverflow  \
+../examples/test_array          \
